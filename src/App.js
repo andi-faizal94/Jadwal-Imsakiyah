df3f44e3ee297117404c67ac2f3cbe418d960880
Andi Faizal <andifaizal647@gmail.com>
2022-03-13 22:10:35 +0700

    clean code

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
         "https://imsakiyah-api.santrikoding.com/imsyakiyah?state=6LoSW8f3n%252F%252FZESP8H%252B5pWRA%252F%252BemLrg67rbWZWm9%252Fx2KZ97lqEpSeRiH94WyxnOQyTNZX%252FEsObmFTFtc26hxbZQ%253D%253D&city=2cpEiccS0pLhJnifcoRhupsrOTAXLHI7hiO7oQWM91iFx9dNB%252FZeF0AG2SleWRgNOI5y6sk6gS8UAHFQOBZDfg%253D%253D&year=2022"
       )
       .then((response) => {
-        console.log(response.data.data);
         setTimes(response.data.data);
       });
   }, []);
