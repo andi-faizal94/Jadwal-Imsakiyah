c7b59a27538a8694d6e813202879b602b91a9363
Andi Faizal <andifaizal647@gmail.com>
2022-03-15 09:14:23 +0700

    add useState loading and error validation

--- src/App.js
+++ src/App.js
@@ -4,16 +4,29 @@ import { Card, Container, Row, Col, Table } from "react-bootstrap";
 
 function App() {
   const [times, setTimes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    setLoading(true);
     axios
       .get(
         "https://imsakiyah-api.santrikoding.com/imsyakiyah?state=6LoSW8f3n%252F%252FZESP8H%252B5pWRA%252F%252BemLrg67rbWZWm9%252Fx2KZ97lqEpSeRiH94WyxnOQyTNZX%252FEsObmFTFtc26hxbZQ%253D%253D&city=2cpEiccS0pLhJnifcoRhupsrOTAXLHI7hiO7oQWM91iFx9dNB%252FZeF0AG2SleWRgNOI5y6sk6gS8UAHFQOBZDfg%253D%253D&year=2022"
       )
       .then((response) => {
         setTimes(response.data.data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
+
+  if (loading)
+    return <h1 className="text-center text-success"> Loading ...</h1>;
+  if (error) console.log(error);
   return (
     <Container
       className="mt-3 mb-3 bg-success
@@ -47,7 +60,7 @@ function App() {
                 </thead>
                 <tbody>
                   {times.map((time, index) => (
-                    <tr key={time.id}>
+                    <tr key={index}>
                       <td className="text-center">{index + 1}</td>
                       <td className="text-center">
                         {index + 1} Ramadhan 1443 H
